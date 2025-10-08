a9665da41a12a0535bf4e038ae55b51efd2f3618
joelAIbarraDino <joel.ibarra@dinozign.com>
2025-10-08 07:36:47 -0600

    personalización de las plantillas y texto

--- resources/js/layouts/settings/Layout.vue
+++ resources/js/layouts/settings/Layout.vue
@@ -12,19 +12,19 @@ import { Link } from '@inertiajs/vue3';
 
 const sidebarNavItems: NavItem[] = [
     {
-        title: 'Profile',
+        title: 'Perfil',
         href: editProfile(),
     },
     {
-        title: 'Password',
+        title: 'Contraseña',
         href: editPassword(),
     },
     {
-        title: 'Two-Factor Auth',
+        title: 'Autenticación 2FA',
         href: show(),
     },
     {
-        title: 'Appearance',
+        title: 'Apariencia',
         href: editAppearance(),
     },
 ];
@@ -35,8 +35,8 @@ const currentPath = typeof window !== undefined ? window.location.pathname : '';
 <template>
     <div class="px-4 py-6">
         <Heading
-            title="Settings"
-            description="Manage your profile and account settings"
+            title="Configuraciones"
+            description="Administra tu perfil y sus configuraciones"
         />
 
         <div class="flex flex-col lg:flex-row lg:space-x-12">
